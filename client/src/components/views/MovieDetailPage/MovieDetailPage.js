7ec0d0674ab9d49ff00593c2036c93dbe9fade93
Jorge  Montoya <jmontoyamor@gmail.com>
2020-10-18 03:29:06 -0700

    18/10/2020

--- client/src/components/views/MovieDetailPage/MovieDetailPage.js
+++ client/src/components/views/MovieDetailPage/MovieDetailPage.js
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { API_URL, API_KEY } from "../../Config";
+import MainImage from '../LandingPage/Sections/MainImage';
+import { IMAGE_URL } from '../../Config';
+
 
 function MovieDetailPage(props) {
+
+
+
+    const [movie, setMovie] = useState([]);
+
+    useEffect(() => {
+        const movieid = props.match.params.movieId
+
+        fetch(`${API_URL}movie/${movieid}/?api_key=${API_KEY}`)
+            .then(response => response.json())
+            .then(response => {
+                console.log(response)
+                setMovie(response)
+            })
+    }, [])
+
     return (
         <div>
+             {movie &&
+                <MainImage image={`${IMAGE_URL}/w1280${movie.backdrop_path}`} title={movie.original_title} text={movie.overview} />
+
+            }
+
             
+
         </div>
     );
 }
--- client/src/components/views/NavBar/NavBar.js
+++ client/src/components/views/NavBar/NavBar.js
@@ -3,6 +3,7 @@ import LeftMenu from './Sections/LeftMenu';
 import RightMenu from './Sections/RightMenu';
 import { Drawer, Button, Icon } from 'antd';
 import './Sections/Navbar.css';
+import Popcorn from '../../../img/popcorn.png';
 
 function NavBar() {
   const [visible, setVisible] = useState(false)
@@ -18,7 +19,9 @@ function NavBar() {
   return (
     <nav className="menu" style={{ position: 'fixed', zIndex: 5, width: '100%' }}>
       <div className="menu__logo">
-        <a href="/">Logo</a>
+        <a href="/">
+          <img src={Popcorn} alt="Logo" style={{height: '2em', width: '2em'}}/>
+        </a>
       </div>
       <div className="menu__container">
         <div className="menu_left">
